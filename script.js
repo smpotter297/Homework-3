34db3dc542dddd1767fd7a8c13bf57a00da0b7c3
smpotter297 <smpotter_297@hotmail.com>
2020-07-26 21:30:42 -0500

    variables created and parameters collected correctly

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
+  
+
+    //collect length of password parameter through prompt as a number type//
+    var passLength = parseInt(prompt("Enter the number of characters between 8 and 128 for your password."));
+    
+    //validate passLength is within parameter requirements //
+    while (passLength < 8 || passLength > 128) {
+      var passLength = parseInt(prompt("Invalid number of characters entered. Enter the number of characters between 8 and 128 for your pssword."));
+    }
+      //collect character type with confirm//
+    var lower = confirm("Do you want to include lower case letters?");
+    var upper = confirm("Do you want to include upper case letters?");
+    var nums = confirm("Do you want to include numbers?");
+    var special = confirm("Do you want to include special characters?");
+    
+    //validate at least one true type exists//
+    while (! (lower || upper || nums || special))
+       {
+        alert("Please enter at least one type of character.");
+        var lower = confirm("Do you want to include lower case letters?");
+        var upper = confirm("Do you want to include upper case letters?");
+        var nums = confirm("Do you want to include numbers?");
+        var special = confirm("Do you want to include special characters?");
+       }   
+      
+    //varf testing  
+    //console.log(lower,upper,nums,special);
+
+    // function to generate the password //
+    function generatePassword() {    
+  }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
